57602c64402698772598d04a50128e2108c0ac13
PedroAzevedo97 <j.pedro.azevedo@tecnico.ulisboa.pt>
2020-12-02 17:39:23 +0000

    Changes to calculate distances

--- hardware/src/iob_knn.v
+++ hardware/src/iob_knn.v
@@ -25,16 +25,71 @@ module iob_knn
    //write signal
    `SIGNAL(write, 1) 
    `COMB write = | wstrb;
+   
+   `SIGNAL(x1, DATA_W) //internal y
+   `SIGNAL(x2, DATA_W) //internal y
+   `SIGNAL(y1, DATA_W) //internal y
+   `SIGNAL(y2, DATA_W) //internal y
 
    //
    //BLOCK 64-bit time counter & Free-running 64-bit counter with enable and soft reset capabilities
    //
    `SIGNAL_OUT(KNN_VALUE, 2*DATA_W)
+   reg [2*DATA_W-1:0]  distances[99:0];
+   //`SIGNAL(distances, 2*DATA_W) [99:0]
+   reg [2*DATA_W-1:0]  bubble; 
+   
+   integer i, j;
+
+   initial begin
+      $dumpfile("knn.vcd");
+      $dumpvars();
+      //en=0;
+      
+      //GERAR PONTOS E CALCULAR DISTANCIAS
+      for (i=0; i<100; i=i+1) begin
+         @(posedge clk) #1 
+         //en=1; 
+         x1={$random}%1000; 
+         x2={$random}%1000;
+         y1={$random}%1000; 
+         y2={$random}%1000;
+         #1
+         distances[i]=KNN_VALUE;
+         $display("Coordenates x1: %d  x2: %d  y1: %d  y2: %d Distance %d",x1, x2, y1, y2, KNN_VALUE);
+      end
+      
+      
+      //ORDENAR DISTANCIAS
+      for (i=0; i<99; i=i+1) begin 
+        for(j=0; j<99-i; j=j+1) begin
+          if(distances[j]>distances[j+1]) begin
+            bubble=distances[j];
+            distances[j]=distances[j+1];
+            distances[j+1]=bubble;
+          end
+        end
+      end
+      
+          
+      for (i=0; i<100; i=i+1)
+        $display("Distance %d: %d", i+1, distances[i]);
+        
+
+      @(posedge clk) #100 $finish;
+
+   end 
+   
    knn_core knn0
      (
       .KNN_ENABLE(KNN_ENABLE),
       .clk(clk),
-      .rst(rst_int)
+      .rst(rst_int),
+      .x1(x1),
+      .x2(x2),
+      .y1(y1),
+      .y2(y2),
+      .z(KNN_VALUE)
       );
    
    
@@ -47,6 +102,9 @@ module iob_knn
    //rdata signal
    //`COMB begin
    //end
+   
       
 endmodule
+
+
 
--- hardware/src/knn.v
+++ hardware/src/knn.v
@@ -8,9 +8,24 @@ module knn_core
    (
     `INPUT(KNN_ENABLE, 1),    
     `INPUT(clk, 1),
-    `INPUT(rst, 1)
+    `INPUT(rst, 1),
+    `INPUT(x1, DATA_W),
+    `INPUT(x2, DATA_W),
+    `INPUT(y1, DATA_W),
+    `INPUT(y2, DATA_W),
+    `OUTPUT(z, 2*DATA_W)
     );
-
+   
+   `SIGNAL(dist_int, 2*DATA_W) //internal y
+   `SIGNAL(distx, 2*DATA_W) //internal y
+   `SIGNAL(disty, 2*DATA_W) //internal y
+   `SIGNAL2OUT(z, dist_int) //connect internal y to output
+   
+   `COMB begin
+     distx=x1-x2;
+     disty=y1-y2;
+     dist_int=distx*distx+disty*disty; 
+   end
 
       
 endmodule
